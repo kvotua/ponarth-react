99a292a57d01a752b8cca00da034cd4af447dc7f
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-07-02 15:01:01 +0200

    routes have been added

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,12 @@
-import Header from "../components/Header"
-
+import Header from "../components/Header";
+import styles from "./styles/mainpage.module.css";
 
 const MainPage = () => {
   return (
-    <div>
-      <Header/>
+    <div className={styles.wrapper}>
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
