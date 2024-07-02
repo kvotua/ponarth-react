33084643fe7ab90dda9bb5fc20da55905d259a43
Nikita <120781264+Venneg7205@users.noreply.github.com>
2024-07-02 19:33:26 +0200

    Map
    map

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
+import Map from "../components/Map";
 import styles from "./styles/mainpage.module.css";
 
 const MainPage = () => {
   return (
     <div className={styles.wrapper}>
       <Header />
+      <Map />
     </div>
   );
 };
