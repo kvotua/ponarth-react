edfe20792f7491c708b9ce905c2485d746b9c5bf
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-07-03 03:42:32 +0200

    MainPage обновлена

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import Map from "../components/Map";
 import NewHistory from "../components/NewHistory";
 import Partnership from "../components/Partnership/Partnership";
+import RightBar from "../components/RightBar";
 import styles from "./styles/mainpage.module.css";
 const MainPage = () => {
   return (
@@ -13,6 +14,7 @@ const MainPage = () => {
         <Partnership />
         <Map />
       </div>
+      <RightBar />
     </div>
   );
 };
