3fc7310fcd0ca31e824180345d1f4f9b63f25cf8
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-07-03 03:26:26 +0200

    03.07

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,18 @@
 import Header from "../components/Header";
 import Map from "../components/Map";
-
 import NewHistory from "../components/NewHistory";
-import styles from "./styles/mainpage.module.css";
 import Partnership from "../components/Partnership/Partnership";
+import styles from "./styles/mainpage.module.css";
 const MainPage = () => {
   return (
     <div className={styles.wrapper}>
       <Header />
-
+      <div className={styles.content}>
+        {/* Сюда кладем компоненты */}
+        <NewHistory />
+        <Partnership />
+        <Map />
+      </div>
     </div>
   );
 };
