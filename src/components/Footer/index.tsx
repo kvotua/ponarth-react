7f962027c46190783723a7f11c3b4f82cec87ad9
arseniy28 <aars40482@gmail.com>
2024-07-04 11:48:30 +0300

    footer
    add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,32 @@
+
+const Footer = () =>{
+    return(
+        <footer>
+            <div className="all_items_footer">
+      <div className="logo_footer"></div>
+
+      <div className="info_footer">
+        <a className="footerButton" href="">АО "БРАУРЭРАЙ ПОНАРТ"</a>
+        <a className="footerButton" href="">Untpappd</a>
+        <a className="footerButton" href="">ВKонтакте</a>
+        <a className="footerButton" href="">Telegram</a>
+      </div>
+      <div className="info_footer">
+        <a className="footerButton" href="#history">История</a>
+        <a className="footerButton" href="#sorta">Продукт</a>
+        <a className="footerButton" href="#maps">На картах</a>
+        <a className="footerButton" href="#news">Новости</a>
+      </div>
+      <div className="info_footer">
+        <a className="footerButton" href="#partner">Партнерам</a>
+        <a className="footerButton" href="">Хочу в команду</a>
+        <a className="footerButton" href="#excursion">Экскурсии</a>
+        <a className="footerButton" href="">СДЕЛАНО КОМАНДОЙ "PRACTICE"</a> 
+      </div>
+      <div className="practice"></div>
+
+    </div>
+        </footer>
+    )
+}
+export default Footer;
