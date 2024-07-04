9aa46d5854b8a85a67d8849f2fc8ad7650430690
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-07-04 17:26:13 +0200

    theme changer

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,21 +1,40 @@
+import { FC, useState } from "react";
 import Header from "../components/Header";
 import Map from "../components/Map";
 import NewHistory from "../components/NewHistory";
 import Partnership from "../components/Partnership/Partnership";
 import RightBar from "../components/RightBar";
 import styles from "./styles/mainpage.module.css";
-const MainPage = () => {
+import { ThemeContext } from "../components/RightBar";
+
+const MainPage: FC = () => {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === "light" ? "dark" : "light";
+      document.body.style.backgroundColor =
+        newTheme === "dark" ? "#121212" : "#fff";
+      return newTheme;
+    });
+  };
+
   return (
-    <div className={styles.wrapper}>
-      <Header />
-      <div className={styles.content}>
-        {/* Сюда кладем компоненты */}
-        <NewHistory />
-        <Map />
-        <Partnership />
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div
+        className={
+          styles.wrapper +
+          (theme === "dark" ? " " + styles.dark + " " + styles.darkWrapper : "")
+        }
+      >
+        <Header />
+        <div className={styles.content}>
+          <NewHistory />
+          <Map />
+          <Partnership />
+        </div>
+        <RightBar />
       </div>
-      <RightBar />
-    </div>
+    </ThemeContext.Provider>
   );
 };
 
