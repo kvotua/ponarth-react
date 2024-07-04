bc1ca1bd94b79bc6e230a96a989b76e8ec0332a2
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-07-04 17:35:54 +0200

    upd_04.07

--- src/components/LookingPage/LookingPage.tsx
+++ src/components/LookingPage/LookingPage.tsx
@@ -1,15 +1,15 @@
-import styles from './LookingPage.module.css';
+import styles from "./LookingPage.module.css";
 
-const LookingPage = () => {
+const Looking = () => {
   return (
     <div className={styles.poisk}>
-    <div className={styles.text_five}>
-      <h3 className={styles.text}>МЫ В ПОИСКЕ СОТРУДНИКОВ</h3>
+      <div className={styles.text_five}>
+        <h1 className={styles.text}>МЫ В ПОИСКЕ СОТРУДНИКОВ</h1>
+      </div>
+
+      <img className={styles.man} src="./src/assets/bREWING.png" alt="" />
     </div>
+  );
+};
 
-    <img className={styles.man}src="./src/assets/bREWING.png" alt="" />
-  </div>
-  )
-}
-
-export default LookingPage
+export default Looking;
