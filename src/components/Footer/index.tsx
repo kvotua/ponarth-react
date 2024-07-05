bd61011db6911febff22933806854369c062a7af
arseniy28 <aars40482@gmail.com>
2024-07-06 00:38:09 +0300

    finish_footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,26 +1,27 @@
+import styles from "./footer.module.css";
 const Footer = () =>{
     return(
         <footer>
-            <div className="{styles.all_items_footer}">
-      <div className="{styles.logo_footer}"></div>
+            <div className={styles.all_items_footer}>
+      <div className={styles.logo_footer}></div>
 
-      <div className="{styles.info_footer}">
-        <a className="{styles.footerButton}" href="">АО "БРАУРЭРАЙ ПОНАРТ"</a>
-        <a className="{styles.footerButton}" href="">Untpappd</a>
-        <a className="{styles.footerButton}" href="">ВKонтакте</a>
-        <a className="{styles.footerButton}" href="">Telegram</a>
+      <div className={styles.info_footer}>
+        <a className={styles.footerButton} href="">АО "БРАУРЭРАЙ ПОНАРТ"</a>
+        <a className={styles.footerButton} href="">Untpappd</a>
+        <a className={styles.footerButton} href="">ВKонтакте</a>
+        <a className={styles.footerButton} href="">Telegram</a>
       </div>
-      <div className="{styles.info_footer}">
-        <a className="{styles.footerButton}" href="#history">История</a>
-        <a className="{styles.footerButton}" href="#sorta">Продукт</a>
-        <a className="{styles.footerButton}" href="#maps">На картах</a>
-        <a className="{styles.footerButton}" href="#news">Новости</a>
+      <div className={styles.info_footer}>
+        <a className={styles.footerButton} href="#history">История</a>
+        <a className={styles.footerButton} href="#sorta">Продукт</a>
+        <a className={styles.footerButton} href="#maps">На картах</a>
+        <a className={styles.footerButton} href="#news">Новости</a>
       </div>
-      <div className="{styles.info_footer}">
-        <a className="{styles.footerButton}" href="#partner">Партнерам</a>
-        <a className="{styles.footerButton}" href="">Хочу в команду</a>
-        <a className="{styles.footerButton}" href="#excursion">Экскурсии</a>
-        <a className="{styles.footerButton}" href="">СДЕЛАНО КОМАНДОЙ "PRACTICE"</a> 
+      <div className={styles.info_footer}>
+        <a className={styles.footerButton} href="#partner">Партнерам</a>
+        <a className={styles.footerButton} href="">Хочу в команду</a>
+        <a className={styles.footerButton} href="#excursion">Экскурсии</a>
+        <a className={styles.footerButton} href="">СДЕЛАНО КОМАНДОЙ "PRACTICE"</a> 
       </div>
       <div className="practice"></div>
 
