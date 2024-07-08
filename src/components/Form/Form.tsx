1c675af4e5bdaa736e7372030ddc678cd543ab37
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-07-08 15:31:37 +0200

    Update Form.tsx

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,39 +1,62 @@
-import styles from './Form.module.css';
+import styles from "./Form.module.css";
 
-const Form = () => {
+const PartnerForm = () => {
   return (
-    <div className={styles.container2} >
-    <video  autoPlay muted loop id="myVideo">   
-      <source src="./src/assets/pexels-cottonbro-5538281.mp4" type="video/mp4" />
-    </video>
-    <div id="block" className={styles.content1}>
-      <h1 className={styles.bochka}>
-        Две гостевые бочки пива <br />
-        каждому новому партнеру от Понарт
-      </h1>
-      <div className={styles.form_four}>
-        <form id="stat_partner">
-          <div className={styles.form_group}>
-            <input type="text" id="userName" name="userName" placeholder=" " required />
-            <label htmlFor="userName">Ваше имя</label>
-          </div>
+    <div className={styles.container2}>
+      <video autoPlay muted loop id="myVideo">
+        <source
+          src="./src/assets/pexels-cottonbro-5538281.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <div id="block" className={styles.content1}>
+        <h1 className={styles.bochka}>
+          Две гостевые бочки пива <br />
+          каждому новому партнеру от Понарт
+        </h1>
+        <div className={styles.form_four}>
+          <form id="stat_partner">
+            <div className={styles.form_group}>
+              <input
+                type="text"
+                id="userName"
+                name="userName"
+                placeholder=" "
+                required
+              />
+              <label htmlFor="userName">Ваше имя</label>
+            </div>
 
-          <div className={styles.form_group}>
-            <input type="tel" id="phoneNumber" maxLength={11} name="phoneNumber" placeholder=" " required />
-            <label htmlFor="phoneNumber">Ваш номер телефона</label>
-          </div>
+            <div className={styles.form_group}>
+              <input
+                type="tel"
+                id="phoneNumber"
+                maxLength={11}
+                name="phoneNumber"
+                placeholder=" "
+                required
+              />
+              <label htmlFor="phoneNumber">Ваш номер телефона</label>
+            </div>
 
-          <button className={styles.learn_more} type="submit" disabled style={{marginTop: 32+'px', width: 250+'px'}}>
-            <span className={styles.circle} aria-hidden="true">
-              <span className={styles.icon_arrow}></span>
-            </span>
-            <span className={styles.button_text} style={{color: 'white'}}>СТАТЬ ПАРТНЕРОМ</span>
-          </button>
-        </form>
+            <button
+              className={styles.learn_more}
+              type="submit"
+              disabled
+              style={{ marginTop: 32 + "px", width: 250 + "px" }}
+            >
+              <span className={styles.circle} aria-hidden="true">
+                <span className={styles.icon_arrow}></span>
+              </span>
+              <span className={styles.button_text} style={{ color: "white" }}>
+                СТАТЬ ПАРТНЕРОМ
+              </span>
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Form
+export default PartnerForm;
