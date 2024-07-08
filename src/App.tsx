9ae1e1fef1d8c562fcc89a9bdd29d8bbcb676c2f
Yaroslav <aars40482@gmail.com>
2024-07-08 16:22:56 +0200

    footer_fix_img
    бывает

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   return (
     // <BrowserRouter>
       <Footer></Footer>
+      
     // </BrowserRouter>
   );
 }
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,36 +1,40 @@
+
 import styles from "./footer.module.css";
-const Footer = () =>{
-    return(
-        <footer>
-            <div className={styles.all_items_footer}>
-      <div >
-        <img className={styles.logo_footer} src="../../assets/big_ponarth_logo_black.png" alt=""/>
-      </div>
+import bigPonarthLogo from "../../assets/big_ponarth_logo_black.png"; // Импорт логотипа
+import practiceLogo from "../../assets/Practice.png"; // Импорт другого логотипа
 
-      <div className={styles.info_footer}>
-        <a className={styles.footerButton} href="">АО "БРАУРЭРАЙ ПОНАРТ"</a>
-        <a className={styles.footerButton} href="">Untpappd</a>
-        <a className={styles.footerButton} href="">ВKонтакте</a>
-        <a className={styles.footerButton} href="">Telegram</a>
-      </div>
-      <div className={styles.info_footer}>
-        <a className={styles.footerButton} href="#history">История</a>
-        <a className={styles.footerButton} href="#sorta">Продукт</a>
-        <a className={styles.footerButton} href="#maps">На картах</a>
-        <a className={styles.footerButton} href="#news">Новости</a>
-      </div>
-      <div className={styles.info_footer}>
-        <a className={styles.footerButton} href="#partner">Партнерам</a>
-        <a className={styles.footerButton} href="">Хочу в команду</a>
-        <a className={styles.footerButton} href="#excursion">Экскурсии</a>
-        <a className={styles.footerButton} href="">СДЕЛАНО КОМАНДОЙ "PRACTICE"</a> 
-      </div>
-      <div >
-        <img className={styles.practice} src="../../assets/Practice.png" alt="" />
-      </div>
+const Footer = () => {
+  return (
+    <footer>
+      <div className={styles.all_items_footer}>
+        <div>
+          <img className={styles.logo_footer} src={bigPonarthLogo} alt="Логотип БРАУРЭРАЙ ПОНАРТ" />
+        </div>
 
-    </div>
-        </footer>
-    );
+        <div className={styles.info_footer}>
+          <a className={styles.footerButton} href="">АО "БРАУРЭРАЙ ПОНАРТ"</a>
+          <a className={styles.footerButton} href="">Untappd</a>
+          <a className={styles.footerButton} href="">ВКонтакте</a>
+          <a className={styles.footerButton} href="">Telegram</a>
+        </div>
+        <div className={styles.info_footer}>
+          <a className={styles.footerButton} href="#history">История</a>
+          <a className={styles.footerButton} href="#sorta">Продукт</a>
+          <a className={styles.footerButton} href="#maps">На картах</a>
+          <a className={styles.footerButton} href="#news">Новости</a>
+        </div>
+        <div className={styles.info_footer}>
+          <a className={styles.footerButton} href="#partner">Партнерам</a>
+          <a className={styles.footerButton} href="">Хочу в команду</a>
+          <a className={styles.footerButton} href="#excursion">Экскурсии</a>
+          <a className={styles.footerButton} href="">СДЕЛАНО КОМАНДОЙ "PRACTICE"</a> 
+        </div>
+        <div>
+          <img className={styles.practice} src={practiceLogo} alt="Логотип PRACTICE" />
+        </div>
+      </div>
+    </footer>
+  );
 };
-export default Footer; 
+
+export default Footer;
