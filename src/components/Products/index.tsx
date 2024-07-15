df4228bb4ac861bb11eee029b182d8eff882c78c
Artem Kovalenko <ak7527203@gmail.com>
2024-07-15 19:49:07 +0200

    smallostalos

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,21 +1,22 @@
 import styles from "../Products/products.module.css";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 import banka1 from "../../assets/Банки12-01.svg";
 import banka2 from "../../assets/Банки1-01.svg";
 import banka3 from "../../assets/Банки-03.svg";
-
+import banka4 from "../../assets/Банки-02.svg";
 export default function Products() {
   return (
     <div className={styles.wrapper}>
       <Swiper
-        effect={'coverflow'}
         grabCursor={true}
         centeredSlides={true}
-        slidesPerView={'auto'}
+        slidesPerView={3} // Показывать три слайда одновременно
+        spaceBetween={30} // Промежуток между слайдами
         coverflowEffect={{
           rotate: 50,
           stretch: 0,
@@ -23,18 +24,36 @@ export default function Products() {
           modifier: 1,
           slideShadows: true,
         }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '"></span>';
+          },
+        }}
+        navigation={true}
+        loop={true}
+        modules={[EffectCoverflow, Pagination, Navigation]}
         className={styles.images}
       >
         <SwiperSlide>
           <div className={styles.swiperimg}>
-            <img src={banka1} alt=""/>
+            <img src={banka1} alt="" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={styles.swiperimg}>
             <img src={banka2} alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.swiperimg}>
+            <img src={banka4} alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.swiperimg}>
+            <img src={banka3} alt="" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
