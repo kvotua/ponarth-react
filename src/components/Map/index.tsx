2f9465121c9240099205e48a7ec3fc01ce55c4b2
Nikita <120781264+Venneg7205@users.noreply.github.com>
2024-07-22 15:07:59 +0200

    Карта с меткой

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,9 +1,32 @@
 import React, { useRef, useState, MouseEvent } from "react";
 import styles from "../Map/map.module.css";
+import { YMaps, Map as YMapsMap, Placemark } from "@pbe/react-yandex-maps";
 
-const Map: React.FC = () => {
+const PonarthMap: React.FC = () => {
   const [isActive, setIsActive] = useState<boolean>(true);
   const mapOverlayRef = useRef<HTMLDivElement>(null);
+
+  const createPlacemark = () => {
+    const center = [54.68113632965351,20.487322667137775];
+    const iconContent = "1";
+
+
+    return (
+      <YMaps>
+        <YMapsMap defaultState={{ center, zoom: 18 }} className={styles.map}>
+          <Placemark
+            geometry={center}
+            properties={{ iconContent,}}
+            options={{
+              iconLayout: "default#image",
+              iconImageHref: "src/assets/Tag_Ponart.svg", // Укажите путь к вашему изображению
+              iconImageSize: [64, 64], // Укажите размер вашего изображения
+            }}
+          />
+        </YMapsMap>
+      </YMaps>
+    );
+  };
 
   const activateMap = (): void => {
     setIsActive(false);
@@ -25,37 +48,24 @@ const Map: React.FC = () => {
       <div className={styles.six_page} id="maps">
         <div className={styles.content_six_page}>
           <div className={styles.div_text}>
-          <h2 className={styles.texter_six}>
-            ПОНАРТ <br />
-            НА КАРТЕ
-          </h2>
+            <h2 className={styles.texter_six}>
+              ПОНАРТ <br />
+              НА КАРТЕ
+            </h2>
           </div>
-          
-            
-          
-            <div className={styles.map_container}>
-              <iframe
-                className={styles.map}
-                src="https://yandex.ru/map-widget/v1/?um=constructor%3A69e7963fbd932471d4fe17d380227c9a9764eb17e7f5c0f0f24f227e3959a1ba&amp;source=constructor"
-                frameBorder="0"
-              ></iframe>
-              <div
-                ref={mapOverlayRef}
-                className={styles.map_overlay}
-                style={{
-                  opacity: isActive ? "1" : "0",
-                  pointerEvents: isActive ? "auto" : "none",
-                }}
-                onClick={activateMap}
-              >
-                Нажмите чтобы увидеть карту
-              </div>
+          <div className={styles.map_container}>
+            <div className={styles.map_overlay} ref={mapOverlayRef} style={{
+              opacity: isActive ? "1" : "0",
+              pointerEvents: isActive ? "auto" : "none",
+            }} onClick={activateMap}>
+              Нажмите чтобы увидеть карту
             </div>
+            {createPlacemark()}
           </div>
         </div>
       </div>
-    
+    </div>
   );
 };
 
-export default Map;
+export default PonarthMap;
