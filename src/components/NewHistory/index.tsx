be2cf5efa444bd27179e14c6243e27dac8900f90
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-07-22 21:08:10 +0200

    upd

--- src/components/NewHistory/index.tsx
+++ src/components/NewHistory/index.tsx
@@ -1,23 +1,24 @@
-import styles from './NewHistory.module.scss';
-import DelayedButton from '../Buttons/DelayedButton'
+import styles from "./NewHistory.module.scss";
+import DelayedButton from "../Buttons/DelayedButton";
 function NewHistory() {
-    return (
-        <div className={styles.setDirection}>
-            <div className={styles.texter}>
-                <h2 className={styles.text}>
-                    СТАНЬ ЧАСТЬЮ <br />НОВОЙ ИСТОРИИ
-                </h2>
-            </div>
-                    
-            <div className={styles.mark}>
-                <div className={styles.buy_action}>
-                    <DelayedButton to='' delay={750} className={styles.history_a}>КУПИТЬ АКЦИЮ</DelayedButton>
-                   
-                </div>
-            </div>
-          
+  return (
+    <div className={styles.setDirection}>
+      <div className={styles.texter}>
+        <h2 className={styles.text}>
+          СТАНЬ ЧАСТЬЮ <br />
+          НОВОЙ ИСТОРИИ
+        </h2>
+      </div>
+
+      <div className={styles.mark}>
+        <div className={styles.buy_action}>
+          <DelayedButton to="" delay={750} className={styles.history_a}>
+            КУПИТЬ АКЦИЮ
+          </DelayedButton>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default NewHistory
+export default NewHistory;
