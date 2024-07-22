e58a3b6d62126bfa3771300db47af3f68ef4e685
m4kyv <braun-erik@inbox.ru>
2024-07-22 14:14:47 +0200

    button update

--- src/components/Buttons/DelayedButton/index.tsx
+++ src/components/Buttons/DelayedButton/index.tsx
@@ -11,7 +11,7 @@ interface DelayedButtonProps {
     style?: string;
 }
 
-const DelayedButton: React.FC<DelayedButtonProps> = ({ to, delay, children, className, style }) => {
+const DelayedButton: React.FC<DelayedButtonProps> = ({ to, delay, children, className, style, onClick }) => {
     let styles = stylesDefault;
     if(style == null || style == 'black'){
         styles = stylesDefault;
@@ -38,12 +38,12 @@ const DelayedButton: React.FC<DelayedButtonProps> = ({ to, delay, children, clas
 
     return (
         <button onClick={handleClick} disabled={isClicked} className={className}>
-           <button className={styles.learn_more}>
+           <div className={styles.learn_more} onClick={onClick}>
               <span className={styles.circle} aria-hidden="true">
                 <span className={`${styles.icon} ${styles.arrow}`}></span>
               </span>
               <span className={styles.button_text}>{children}</span>
-            </button>
+            </div>
         </button>
     );
 };
