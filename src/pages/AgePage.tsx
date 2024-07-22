6d03c57584573410c76523fae7075f9e96b1df25
m4kyv <braun-erik@inbox.ru>
2024-07-22 13:54:58 +0200

    button update

--- src/pages/AgePage.tsx
+++ src/pages/AgePage.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 import styles from "./styles/agepage.module.css";
 import logo from "../assets/Ponarth_firmenny_blok_01.svg";
 import DelayedButton from '../components/Buttons/DelayedButton';
-import DelayedButtonWhite from "../components/Buttons/DelayedButtonWhite";
 const AgePage: React.FC<{ setAgeConfirmed: (confirmed: boolean) => void }> = ({
   setAgeConfirmed,
 }) => {
@@ -14,8 +13,8 @@ const AgePage: React.FC<{ setAgeConfirmed: (confirmed: boolean) => void }> = ({
     const ageConfirmedTime = localStorage.getItem("ageConfirmedTime");
     if (
       ageConfirmedTime &&
-      new Date().getTime() - Number(ageConfirmedTime) < 24 * 60 * 60 * 1000
-    ) {
+      new Date().getTime() - Number(ageConfirmedTime) < 24*60*60*1000
+     ) {
       setAgeConfirmed(true);
         navigate("/home");
       
@@ -30,12 +29,14 @@ const AgePage: React.FC<{ setAgeConfirmed: (confirmed: boolean) => void }> = ({
     if (age >= 18) {
       localStorage.setItem("ageConfirmedTime", String(new Date().getTime()));
       setAgeConfirmed(true);
-      let timer: ReturnType<typeof setTimeout>;
-      timer = setTimeout(() => {
+      setTimeout(() => {
         navigate("/home");
     }, 750);
     } else {
-      window.location.href = "https://www.detmir.ru/catalog/index/name/pure/";
+      setTimeout(() => {
+        window.location.href = "https://www.detmir.ru/catalog/index/name/pure/";
+      }, 750);
+     
     }
   };
 
@@ -62,13 +63,7 @@ const AgePage: React.FC<{ setAgeConfirmed: (confirmed: boolean) => void }> = ({
           value={age}
           onChange={handleSliderChange}
         />
-        <DelayedButtonWhite to='' className={styles.history_a} delay={450} onClick={handleConfirmClick}>ПРОДОЛЖИТЬ</DelayedButtonWhite>
-        {/* <button className={styles.learn_more} onClick={handleConfirmClick}>
-          <span className={styles.circle} aria-hidden="true">
-            <span className={`${styles.icon} ${styles.arrow}`}></span>
-          </span>
-          <span className={styles.button_text_two}>ПРОДОЛЖИТЬ</span>
-        </button> */}
+        <DelayedButton to='' className={styles.history_a} delay={450} onClick={handleConfirmClick} style="white">ПРОДОЛЖИТЬ</DelayedButton>
       </div>
     </div>
   );
