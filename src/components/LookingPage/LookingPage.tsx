e448f1dff92aadf35fa493b6053eedeb41c06a4e
kvotua <kvotua.job@gmail.com>
2024-07-24 03:04:49 +0200

    ref

--- src/components/LookingPage/LookingPage.tsx
+++ src/components/LookingPage/LookingPage.tsx
@@ -2,12 +2,8 @@ import styles from "./LookingPage.module.css";
 
 const Looking = () => {
   return (
-    <div className={styles.poisk}>
-      <div className={styles.text_five}>
-        <h1 className={styles.text}>МЫ В ПОИСКЕ СОТРУДНИКОВ</h1>
-      </div>
-
-      <img className={styles.man} src="./src/assets/bREWING.png" alt="" />
+    <div className={styles.div_text}>
+        <h2 className={styles.texter_six}>МЫ В ПОИСКЕ СОТРУДНИКОВ</h2>
     </div>
   );
 };
