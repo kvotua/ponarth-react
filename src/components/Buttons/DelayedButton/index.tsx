cdd93da1f4c796f3fc9a904fcfbe0c6956a6c6b5
m4kyv <braun-erik@inbox.ru>
2024-07-24 18:00:59 +0200

    upd form adaptive

--- src/components/Buttons/DelayedButton/index.tsx
+++ src/components/Buttons/DelayedButton/index.tsx
@@ -53,7 +53,7 @@ const DelayedButton: React.FC<DelayedButtonProps> = ({
   return (
     <button
       onClick={handleClick}
-      disabled={isClicked}
+   //   disabled={isClicked}
       className={className}
       style={mergeStyles}
       type={type}
