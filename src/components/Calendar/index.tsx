e16a801c1789da4e3a289f3faa3ca14696e4a37c
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-07-25 22:12:57 +0200

    upd

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styles from "./calendar.module.css";
 import Calendar, { CalendarProps } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import "./customCalendar.css";
 import { format } from "date-fns";
 import { ru } from "date-fns/locale";
 
@@ -12,14 +13,13 @@ const CalendarComp: React.FC = () => {
     setDate(value as Date);
   };
 
-  useEffect(() => {
-    const yearButtons = document.querySelectorAll(
-      '.react-calendar__navigation button[aria-label*="year"]'
-    );
-    yearButtons.forEach((button) => {
-      button.style.display = "none";
-    });
-  }, []);
+  const tileDisabled = ({ date, view }: { date: Date; view: string }) => {
+    if (view === "month") {
+      const day = date.getDay();
+      return day !== 2 && day !== 3 && day !== 4; // 2 - вторник, 3 - среда, 4 - четверг
+    }
+    return false;
+  };
 
   return (
     <>
@@ -37,15 +37,11 @@ const CalendarComp: React.FC = () => {
         onChange={onChange}
         value={date}
         locale="ru-RU"
-        showNavigation={true}
         formatMonthYear={(locale, date) =>
           format(date, "LLLL yyyy", { locale: ru })
         }
+        tileDisabled={tileDisabled}
       />
-      <p>
-        Выбранная дата:{" "}
-        {date ? format(date, "PPPP", { locale: ru }) : "Не выбрана"}
-      </p>
     </>
   );
 };
