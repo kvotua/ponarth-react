d8968ea1bc87a6bf09b6d723abc252a236fe9bc1
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-07-28 01:01:21 +0200

    upd

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -6,11 +6,11 @@ import "./customCalendar.css";
 import { format } from "date-fns";
 import { ru } from "date-fns/locale";
 import InputMask from "react-input-mask";
+import image1 from "../../assets/Pivo2.png";
 
 const CalendarComp: React.FC = () => {
   const [date, setDate] = useState<Date | null>(new Date());
   const [persons, setPersons] = useState<number>(1);
-
   const onChange: CalendarProps["onChange"] = (value) => {
     setDate(value as Date);
   };
@@ -41,53 +41,77 @@ const CalendarComp: React.FC = () => {
           <br /> СЕРДЦЕ ПРУССИИ
         </p>
       </div>
-
-      <div className={styles.calendar_block}>
-        <p className={styles.mark}>
-          Каждый вторник, среду и четверг <br />
-          12.00 15.00. 19.00
-        </p>
-        <Calendar
-          onChange={onChange}
-          value={date}
-          locale="ru-RU"
-          formatMonthYear={(_, date) =>
-            format(date, "LLLL yyyy", { locale: ru })
-          }
-          tileDisabled={tileDisabled}
-          navigationLabel={({ view, date }) => {
-            if (view === "month") {
-              return (
-                <span className="react-calendar__navigation__label">
-                  {format(date, "LLLL yyyy", { locale: ru })}
-                </span>
-              );
+      <p className={styles.mark}>
+        Каждый вторник, среду и четверг <br />
+        12.00 15.00. 19.00
+      </p>
+      <div className={styles.content}>
+        <div className={styles.calendar_block}>
+          <Calendar
+            onChange={onChange}
+            value={date}
+            locale="ru-RU"
+            formatMonthYear={(_, date) =>
+              format(date, "LLLL yyyy", { locale: ru })
             }
-            return null;
-          }}
-          prev2Label={null} // Убираем стрелку для предыдущего года
-          next2Label={null} // Убираем стрелку для следующего года
-        />
+            tileDisabled={tileDisabled}
+            navigationLabel={({ view, date }) => {
+              if (view === "month") {
+                return (
+                  <span className="react-calendar__navigation__label">
+                    {format(date, "LLLL yyyy", { locale: ru })}
+                  </span>
+                );
+              }
+              return null;
+            }}
+            prev2Label={null} // Убираем стрелку для предыдущего года
+            next2Label={null} // Убираем стрелку для следующего года
+          />
 
-        <input
-          className={styles.input_calendar}
-          type="text"
-          placeholder="Ваше имя"
-        />
-        <InputMask
-          className={styles.input_calendar}
-          mask="+7 (999) 999-99-99"
-          placeholder="Контактный номер телефона"
-        />
-        <div className={styles.input_calendar}>
-          <label>Количество персон</label>
-          <div className={styles.persons_input}>
-            <button onClick={handleDecrement}>—</button>
-            <input type="text" value={persons} readOnly />
-            <button onClick={handleIncrement}>+</button>
+          <input
+            className={styles.input_calendar}
+            type="text"
+            placeholder="Ваше имя"
+          />
+          <InputMask
+            className={styles.input_calendar}
+            mask="+7 (999) 999-99-99"
+            placeholder="Контактный номер телефона"
+          />
+          <div className={styles.input_calendar}>
+            <label>Количество персон</label>
+            <div className={styles.persons_input}>
+              <button onClick={handleDecrement}>—</button>
+              <input type="text" value={persons} readOnly />
+              <button onClick={handleIncrement}>+</button>
+            </div>
+          </div>
+          <button className={styles.reserve_btn}>Забронировать</button>
+        </div>
+
+        <div className={styles.photos_block}>
+          <div className={styles.slides}>
+            <div className={styles.slide}>
+              <img src={image1} alt="" />
+              <p className={styles.slide_p}>
+                #Знакомство с культурным наследием
+              </p>
+            </div>
+            <div className={styles.slide}>
+              <img src={image1} alt="" />
+              <p className={styles.slide_p}>
+                #Знакомство с культурным наследием
+              </p>
+            </div>
+            <div className={styles.slide}>
+              <img src={image1} alt="" />
+              <p className={styles.slide_p}>
+                #Знакомство с культурным наследием
+              </p>
+            </div>
           </div>
         </div>
-        <button className={styles.reserve_btn}>Забронировать</button>
       </div>
     </>
   );
