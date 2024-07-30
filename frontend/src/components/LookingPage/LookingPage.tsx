64e68c459bb39794b915cedb8511d0437dc9e506
Quoicke <dan4k2345@gmail.com>
2024-07-30 11:26:55 +0200

    task/944-реализовать-блок-вакансии2

--- frontend/src/components/LookingPage/LookingPage.tsx
+++ frontend/src/components/LookingPage/LookingPage.tsx
@@ -1,10 +1,15 @@
 import styles from "./LookingPage.module.css";
+import Vakansii from "../Vakansii";
 
 const Looking = () => {
   return (
+    <>
     <div className={styles.div_text}>
-        <h2 className={styles.texter_six}>МЫ В ПОИСКЕ СОТРУДНИКОВ</h2>
+        <h2 className={styles.texter_six}>МЫ В ПОИСКЕ <br /> СОТРУДНИКОВ</h2>
     </div>
+    <Vakansii/>
+    </>
+    
   );
 };
 
