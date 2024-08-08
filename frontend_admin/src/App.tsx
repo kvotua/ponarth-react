b801593e23be123d117647299f81cb4775f4b0f5
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-08-08 19:35:08 +0200

    upd

--- frontend_admin/src/App.tsx
+++ frontend_admin/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import ProductPage from './pages/ProductsPage'
@@ -9,28 +10,33 @@ import AddPointsPage from './pages/AddPointPage'
 import AddTelegramNotificationPage from './pages/AddNotificationsPage'
 import AddLayout from './components/AddLayout'
 import AddProductPage from './pages/AddProductPage'
+import TokenValidator from './shared/tokenValidator'
 
-function App() {
+const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        {/* Parent routes */}
-        <Route path="/" element={<Layout />}>
-          <Route path="products" element={<ProductPage />} />
-          <Route path="vacancies" element={<VacanciesPage />} />
-          <Route path="points" element={<PointsPage />} />
-          <Route path="notifications" element={<NotificationPage />} />
-        </Route>
+      <TokenValidator>
+        <Routes>
+          {/* Parent routes */}
+          <Route path="/" element={<Layout />}>
+            <Route path="products" element={<ProductPage />} />
+            <Route path="vacancies" element={<VacanciesPage />} />
+            <Route path="points" element={<PointsPage />} />
+            <Route path="notifications" element={<NotificationPage />} />
+          </Route>
 
-        {/* Child routes */}
-        <Route path="/" element={<AddLayout />}>
-          <Route path="vacancies/add" element={<AddVacanciesPage />} />
-          <Route path="points/add" element={<AddPointsPage />} />
-          <Route path="products/add" element={<AddProductPage />} />
-          <Route path="notifications/add"element={<AddTelegramNotificationPage />}
-          />
-        </Route>
-      </Routes>
+          {/* Child routes */}
+          <Route path="/" element={<AddLayout />}>
+            <Route path="vacancies/add" element={<AddVacanciesPage />} />
+            <Route path="points/add" element={<AddPointsPage />} />
+            <Route path="products/add" element={<AddProductPage />} />
+            <Route
+              path="notifications/add"
+              element={<AddTelegramNotificationPage />}
+            />
+          </Route>
+        </Routes>
+      </TokenValidator>
     </Router>
   )
 }
