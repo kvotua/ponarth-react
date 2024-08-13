c40bc3b58df3a1b38f35c9714f57c51c320b4e28
kvotua <kvotua.job@gmail.com>
2024-08-13 17:23:21 +0200

    added update image backend

--- backend/src/main/java/com/solomennicova/AuthTemplate/Controller/AdminController.java
+++ backend/src/main/java/com/solomennicova/AuthTemplate/Controller/AdminController.java
@@ -1,10 +1,7 @@
 package com.solomennicova.AuthTemplate.Controller;
 
 import com.solomennicova.AuthTemplate.Dto.Exception.ErrorDto;
-import com.solomennicova.AuthTemplate.Dto.Site.BeerDto;
-import com.solomennicova.AuthTemplate.Dto.Site.BeerInfoDto;
-import com.solomennicova.AuthTemplate.Dto.Site.VacancyDto;
-import com.solomennicova.AuthTemplate.Dto.Site.VacancyInfoDto;
+import com.solomennicova.AuthTemplate.Dto.Site.*;
 import com.solomennicova.AuthTemplate.Exception.BeerNotFoundException;
 import com.solomennicova.AuthTemplate.Exception.DontImageException;
 import com.solomennicova.AuthTemplate.Exception.ImageDontLoadException;
@@ -20,6 +17,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @CrossOrigin
 @RestController
@@ -64,8 +63,20 @@ public class AdminController {
     })
     @SecurityRequirement(name = "Bearer Authentication")
     @PutMapping("/beer/update")
-    public void updateBeer(@RequestBody BeerInfoDto beerUpdate) throws BeerNotFoundException {
+    public void updateBeer(@RequestBody BeerUpdateInfoDto beerUpdate) throws BeerNotFoundException {
         siteService.updateBeer(beerUpdate);
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = {
+                    @Content(schema = @Schema(implementation = ErrorDto.class))
+            })
+    })
+    @SecurityRequirement(name = "Bearer Authentication")
+    @PutMapping(path="/beer/image/update/{id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public void updateImageBeer(@PathVariable Long id , @RequestPart("file") final MultipartFile file) throws BeerNotFoundException, IOException {
+        siteService.updateImageBeer(id, file);
     }
 
     @ApiResponses(value = {
@@ -112,8 +123,20 @@ public class AdminController {
     })
     @SecurityRequirement(name = "Bearer Authentication")
     @PutMapping("/vacancy/update")
-    public void updateVacancy(@RequestBody VacancyInfoDto vacancyInfoDto) throws VacancyNotFoundException {
+    public void updateVacancy(@RequestBody VacancyUpdateInfoDto vacancyInfoDto) throws VacancyNotFoundException {
         siteService.updateVacancy(vacancyInfoDto);
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = {
+                    @Content(schema = @Schema(implementation = ErrorDto.class))
+            })
+    })
+    @SecurityRequirement(name = "Bearer Authentication")
+    @PutMapping(path="/vacancy/image/update/{id}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public void updateImageVacancy(@PathVariable Long id , @RequestPart("file") final MultipartFile file) throws VacancyNotFoundException, IOException {
+        siteService.updateImageVacancy(id, file);
     }
 
     @ApiResponses(value = {
