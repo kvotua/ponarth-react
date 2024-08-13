4a9f7454a590e5e0ccca6bc03c897beae72c72b7
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-08-13 19:45:04 +0200

    upd

--- frontend/src/components/Slider/SliderVakansii.tsx
+++ frontend/src/components/Slider/SliderVakansii.tsx
@@ -1,56 +1,43 @@
-import React, { useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import pivovar from './img/hova.png';
-import kovar from './img/ilya.png';
-import novar from './img/Leha.png';
-import dovar from './img/Eji.png';
+import "swiper/css";
+import "swiper/css/effect-cards";
 
-import 'swiper/css';
-import 'swiper/css/effect-cards';
+import stylesV from "./Vaksnsii.module.scss";
+import styles from "./styles.module.css";
 
-import stylesV from './Vaksnsii.module.scss';
-import styles from './styles.module.css';
-
-import { EffectCards } from 'swiper/modules';
-import DelayedButton from '../Buttons/DelayedButton';
-import InputMask from 'react-input-mask';
+import { EffectCards } from "swiper/modules";
+import DelayedButton from "../Buttons/DelayedButton";
+import InputMask from "react-input-mask";
 import { ThemeContext } from "../RightBar";
 import { useContext } from "react";
-const cars = [
-  {
-    id: 1,
-    title: 'БРИГАДИР СМЕНЫ',
-    opisanie: 'Контроль производственных процессов, управление командой на линии.',
-    image: pivovar,
-  },
-  {
-    id: 2,
-    title: 'ОПЕРАТОР ОБОРУДОВАНИЯ',
-    opisanie: 'Настройка и обслуживание пивоваренного оборудования.',
-    image: kovar,
-  },
-  {
-    id: 3,
-    title: 'ТЕХНОЛОГ ПИВОВАРЕНИЯ',
-    opisanie: 'Разработка рецептур, контроль качества готовой продукции.',
-    image: novar,
-  },
-  {
-    id: 4,
-    title: 'ЛАБОРАНТ-МИКРОЛОГ',
-    opisanie: 'Проведение анализа сырья и готового пива.',
-    image: dovar,
-  },
-];
+import { getVacancies, Vacancy } from "../../api/vacancies";
 
 const SliderVakansii: React.FC = () => {
   const { theme } = useContext(ThemeContext);
   const themeButton = theme === "dark" ? "white" : "mixed";
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [vacancies, setVacancies] = useState<Vacancy[]>([]);
+
+  useEffect(() => {
+    const fetchVacancies = async () => {
+      try {
+        const data = await getVacancies();
+        const vacanciesWithDecodedImages = data.map((vacancy) => ({
+          ...vacancy,
+          base64Image: `data:image/jpeg;base64,${vacancy.image}`,
+        }));
+        setVacancies(vacanciesWithDecodedImages);
+      } catch (error) {
+        console.error("Error fetching vacancies", error);
+      }
+    };
+    fetchVacancies();
+  }, []);
 
   return (
-    <div className={stylesV.app} >
+    <div className={stylesV.app}>
       <div className={stylesV.swiper_container}>
         <Swiper
           effect="cards"
@@ -59,21 +46,23 @@ const SliderVakansii: React.FC = () => {
           className={styles.swiper}
           onSlideChange={(swiper) => setCurrentIndex(swiper.activeIndex)}
           scrollbar={false}
-          
         >
-          {cars.map((car) => (
-            <SwiperSlide key={car.id} className={styles.swiper_slide}>
-              <img src={car.image} alt={car.title} />
+          {vacancies.map((vacancy) => (
+            <SwiperSlide key={vacancy.id} className={styles.swiper_slide}>
+              <img src={vacancy.base64Image} alt={vacancy.name} />
             </SwiperSlide>
           ))}
         </Swiper>
       </div>
       <div className={stylesV.text_container}>
-      <h1>{cars[currentIndex].title}</h1>
-      <p>{cars[currentIndex].opisanie}</p>
+        <h1>{vacancies[currentIndex]?.name}</h1>
+        <p>{vacancies[currentIndex]?.description}</p>
         <form id="stat_partner" className={stylesV.stat_partner}>
-        
-          <div className={`${stylesV.form_group} ${theme === "dark" ? stylesV.dark : ""}`}>
+          <div
+            className={`${stylesV.form_group} ${
+              theme === "dark" ? stylesV.dark : ""
+            }`}
+          >
             <input
               type="text"
               id="userName"
@@ -85,7 +74,11 @@ const SliderVakansii: React.FC = () => {
             <label htmlFor="userName">Ваше имя</label>
           </div>
 
-          <div className={`${stylesV.form_group} ${theme === "dark" ? stylesV.dark : ""}`}>
+          <div
+            className={`${stylesV.form_group} ${
+              theme === "dark" ? stylesV.dark : ""
+            }`}
+          >
             <InputMask
               className={stylesV.input}
               name="phoneNumber"
@@ -97,14 +90,19 @@ const SliderVakansii: React.FC = () => {
             />
             <label htmlFor="phoneNumber">Ваш номер телефона</label>
           </div>
-          
+
           <DelayedButton
             type="submit"
             to=""
             className={stylesV.learn_more}
             style={themeButton}
             delay={450}
-            dopstyle={{margin:'0 auto', alignContent:'center', marginTop:  '20px', width: '100%' }}
+            dopstyle={{
+              margin: "0 auto",
+              alignContent: "center",
+              marginTop: "20px",
+              width: "100%",
+            }}
           >
             ОСТАВИТЬ ЗАЯВКУ
           </DelayedButton>
@@ -112,6 +110,6 @@ const SliderVakansii: React.FC = () => {
       </div>
     </div>
   );
-}
+};
 
 export default SliderVakansii;
