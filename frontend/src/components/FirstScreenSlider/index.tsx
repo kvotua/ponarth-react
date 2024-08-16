91f48b81847d7723168f00c6367c3c2e4539747d
m4kyv <braun-erik@inbox.ru>
2024-08-16 13:40:38 +0200

    fix pagination

--- frontend/src/components/FirstScreenSlider/index.tsx
+++ frontend/src/components/FirstScreenSlider/index.tsx
@@ -1,29 +1,20 @@
-import { useContext, useState, useEffect} from "react";
-import styles from "./FirstScreenSlider.module.css";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "./swiper.css";
-import "./swiperNavigation.css";
-import "./swiperPagination.css";
-import { Pagination, Navigation, EffectCoverflow } from "swiper/modules";
-import pivoImg from "../../assets/bottle1-01.svg";
-import pivoImg2 from "../../assets/bottle2-01.svg";
-import pivoImg3 from "../../assets/bottle3-01.svg";
-import { Swiper as SwiperType } from 'swiper/types';
-import DelayedButton from "../Buttons/DelayedButton";
+import styles from './FirstScreenSlider.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useContext, useState, useEffect } from "react";
+import './swiper.css';
+import './swiperNavigation.css';
+import './swiperPagination.css';
+import { Pagination, Navigation, EffectCoverflow } from 'swiper/modules';
+import pivoImg from '../../assets/bottle1-01.svg';
+import pivoImg2 from '../../assets/bottle2-01.svg';
+import pivoImg3 from '../../assets/bottle3-01.svg';
+import DelayedButton from '../Buttons/DelayedButton';
 import { ThemeContext } from "../RightBar";
-
-// interface Product {
-//   id: number;
-//   name: string;
-//   description: string;
-//   color: string;
-//   image: string;
-//   base64Image: string;
-// }
-
+import { Swiper as SwiperType } from 'swiper/types';
 const FirstScreenSlider = () => {
   const [swiperInstance, setSwiperInstance] = useState<SwiperType | null>(null);
   const [activeIndex, setActiveIndex] = useState(0);
+
   const handleSlideChange = (swiper : SwiperType) => {
     setActiveIndex(swiper.realIndex);
   };
@@ -37,84 +28,66 @@ const FirstScreenSlider = () => {
   }, [activeIndex, swiperInstance]);
   const { theme } = useContext(ThemeContext);
   const themeButton = theme === "dark" ? "white" : "mixed";
-
-  // useEffect(() => {
-  //   const fetchProducts = async () => {
-  //     try {
-  //       const data = await getProducts();
-  //       const productsWithDecodedImages = data.map((product) => ({
-  //         ...product,
-  //         base64Image: `data:image/jpeg;base64,${product.image}`,
-  //       }));
-  //       setProducts(productsWithDecodedImages);
-  //     } catch (error) {
-  //       console.error("Error fetching products", error);
-  //     }
-  //   };
-  //   fetchProducts();
-  // }, []);
-
   return (
-    <>
-    <div className={styles.first_container} content="f" id="sorta">
-      <Swiper 
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={3}
-        spaceBetween={0}
-         onSlideChange={handleSlideChange}
+    <div className={styles.first_container} content='f' id='sorta'>
+        <Swiper
+     effect={'coverflow'}
+     onSlideChange={handleSlideChange}
      onSwiper={setSwiperInstance}
-        loop={true}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 20,
-          depth: 250,
-          modifier: 1,
-          slideShadows: false,
-          scale: 1,
-        }}
-        breakpoints={{
-          769: {
-            slidesPerView: 3,
-            coverflowEffect: {
-              rotate: 0,
-              stretch: 0,
-              depth: 250,
-              modifier: 1,
-              slideShadows: false,
-            },
+    grabCursor={true}
+    centeredSlides={true}
+    slidesPerView={3}
+    spaceBetween={0}
+    loop={true}
+    coverflowEffect={{
+        rotate: 0,
+        stretch: 20,
+        depth: 250,
+        modifier: 1,
+        slideShadows: false,
+        scale:1,
+      }}
+      breakpoints={{
+        769:{
+slidesPerView:3,
+coverflowEffect: {
+  rotate: 0,
+  stretch: 0,
+  depth: 250,
+  modifier: 1,
+  slideShadows: false,
+},
+        },
+        600:{
+          slidesPerView: 1,
+          coverflowEffect: {
+            rotate: 0,
+            stretch: 420,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
           },
-          600: {
-            slidesPerView: 1,
-            coverflowEffect: {
-              rotate: 0,
-              stretch: 420,
-              depth: 100,
-              modifier: 1,
-              slideShadows: false,
-            },
+        },
+        500:{
+          slidesPerView: 1,
+          coverflowEffect: {
+            rotate: 0,
+            stretch: 300,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
           },
-          500: {
-            slidesPerView: 1,
-            coverflowEffect: {
-              rotate: 0,
-              stretch: 300,
-              depth: 100,
-              modifier: 1,
-              slideShadows: false,
-            },
+        },
+        320: {
+          slidesPerView: 1,
+          coverflowEffect: {
+            rotate: 0,
+            stretch: 190,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
           },
-          320: {
-            slidesPerView: 1,
-            coverflowEffect: {
-              rotate: 0,
-              stretch: 190,
-              depth: 100,
-              modifier: 1,
-              slideShadows: false,
-            },
-          },
+        },
       }}
     pagination={{
       el: '.custom-pagination',
@@ -129,30 +102,32 @@ const FirstScreenSlider = () => {
     className={styles.swiper} 
     content='f'
   >
-    
     <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
     <SwiperSlide className={` ${styles.default} ${styles.swiper_slide}`} content='f'><img src={pivoImg2} alt="" /></SwiperSlide>
     <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
-    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
-    <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
-    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
-    <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
-    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
-    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
-    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide}`} content='f'><img src={pivoImg2} alt="" /></SwiperSlide>
-    <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
-    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
-    <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
-    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
-    <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
-    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
-    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
-    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide}`} content='f'><img src={pivoImg2} alt="" /></SwiperSlide>
-    <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
-    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
+ <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
+ <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
+ <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
+ <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
+ <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
 
- <div className="custom-pagination">   
-  {[...Array(4)].map((_, index) => (
+ <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
+    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide}`} content='f'><img src={pivoImg2} alt="" /></SwiperSlide>
+    <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
+ <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
+ <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
+ <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
+ <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
+ <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
+ <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
+    <SwiperSlide className={` ${styles.default} ${styles.swiper_slide}`} content='f'><img src={pivoImg2} alt="" /></SwiperSlide>
+    <SwiperSlide className={`${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg3} alt="" /></SwiperSlide>
+ <SwiperSlide className={` ${styles.default} ${styles.swiper_slide} `} content='f'><img src={pivoImg} alt="" /></SwiperSlide>
+ <div className="custom-pagination"
+//  style={{
+//   transform: `translateX(${(activeIndex * 0.5)+10}vw)`,
+// }}
+ >   {[...Array(4)].map((_, index) => (
   <div
     key={index}
     className={`swiper-pagination-bullet-first ${activeIndex === index ? 'swiper-pagination-bullet-active-first' : ''}`}
@@ -169,14 +144,13 @@ const FirstScreenSlider = () => {
           </p>
           <a href="#maps" className={styles.button_slider}>
           <DelayedButton to='' delay={1} className={styles.button_slider} style={themeButton}>ГДЕ ПОПРОБОВАТЬ?</DelayedButton>
-
           </a>
-        </div>
-      </div>
+    </div>
+          
+</div>
     </div>
     </div>
-    </>
-  );
-};
+  )
+}
 
-export default FirstScreenSlider;
+export default FirstScreenSlider
