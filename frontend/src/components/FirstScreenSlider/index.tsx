a6b4e4cf5571373c01f9a898a261193b2a424b41
Nikita Gilevski <125219872+gilevski-tla-dev@users.noreply.github.com>
2024-08-19 16:56:27 +0200

    Update index.tsx

--- frontend/src/components/FirstScreenSlider/index.tsx
+++ frontend/src/components/FirstScreenSlider/index.tsx
@@ -5,33 +5,31 @@ import { Swiper as SwiperType } from "swiper/types";
 import styles from "./FirstScreenSlider.module.css";
 import DelayedButton from "../Buttons/DelayedButton";
 import { ThemeContext } from "../RightBar";
-import { getProducts, Products } from "../../api/products"; // Import the getProducts function
+import { getProducts, Products } from "../../api/products";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-import img1 from "../../assets/Pivo2.png";
-import img2 from "../../assets/Pivo3.png";
-import img3 from "../../assets/photo_2024-08-18_13-44-12.jpg";
-import img4 from "../../assets/photo_2024-08-18_13-44-13.jpg";
-import img5 from "../../assets/bottle1-01.svg";
-import img6 from "../../assets/bottle2-01.svg";
-import img7 from "../../assets/bottle3-01.svg";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const FirstScreenSlider = () => {
   const [swiperInstance, setSwiperInstance] = useState<SwiperType | null>(null);
-  // const [activeIndex, setActiveIndex] = useState(0);
-  // const [products, setProducts] = useState<Products[]>([]); // State to store products
-  const [currentProduct, setCurrentProduct] = useState<Products | null>(null); // State to store current product
+  const [currentProduct, setCurrentProduct] = useState<Products | null>(null);
   const [stretch, setStretch] = useState<number>(190);
+  const [images, setImages] = useState<{ id: string; src: string }[]>([]);
+  const [productsData, setProductsData] = useState<Products[]>([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const productsData = await getProducts();
-        // setProducts(productsData);
-        setCurrentProduct(productsData[0]); // Set the first product as the current product initially
+        const products = await getProducts();
+        setProductsData(products);
+        setCurrentProduct(products[0]);
+
+        const imagesData = products.map((product, index) => ({
+          id: `img${index + 1}`,
+          src: getImageSrc(product.image, product.fileName),
+        }));
+        setImages(imagesData);
       } catch (error) {
         console.error("Error fetching products", error);
       }
@@ -40,130 +38,134 @@ const FirstScreenSlider = () => {
     fetchProducts();
   }, []);
 
-
   const { theme } = useContext(ThemeContext);
   const themeButton = theme === "dark" ? "white" : "mixed";
 
-  // const getImageSrc = (image: string, fileName: string) => {
-  //   const extension = fileName.split(".").pop()?.toLowerCase();
-  //   switch (extension) {
-  //     case "svg":
-  //       return `data:image/svg+xml;base64,${image}`;
-  //     case "png":
-  //       return `data:image/png;base64,${image}`;
-  //     case "jpeg":
-  //     case "jpg":
-  //       return `data:image/jpeg;base64,${image}`;
-  //     default:
-  //       return "";
-  //   }
-  // };
+  const getImageSrc = (image: string, fileName: string) => {
+    const extension = fileName.split(".").pop()?.toLowerCase();
+    switch (extension) {
+      case "svg":
+        return `data:image/svg+xml;base64,${image}`;
+      case "png":
+        return `data:image/png;base64,${image}`;
+      case "jpeg":
+      case "jpg":
+        return `data:image/jpeg;base64,${image}`;
+      default:
+        return "";
+    }
+  };
 
-  const images: { id: string; src: string }[] = [
-    { id: 'img1', src: img1 },
-    { id: 'img2', src: img2 },
-    { id: 'img3', src: img3 },
-    { id: 'img4', src: img4 },
-    { id: 'img5', src: img5 },
-    { id: 'img6', src: img6 },
-    { id: 'img7', src: img7 },
-];
-  const [visibleSlides, setVisibleSlides] = useState<string[]>([]); 
+  const [visibleSlides, setVisibleSlides] = useState<string[]>([]);
 
   const handleSlideChange = (swiper: SwiperType) => {
-    const visible = swiper.slides.filter((slide: HTMLElement) => 
-        slide.classList.contains('swiper-slide-visible')
+    const visible = swiper.slides.filter((slide: HTMLElement) =>
+      slide.classList.contains("swiper-slide-visible")
     );
-    setVisibleSlides(visible.map((slide) => slide.querySelector('img')?.getAttribute('data-id') || ''));
-};
-const [isSafari, setIsSafari] = useState(false);
+    setVisibleSlides(
+      visible.map(
+        (slide) => slide.querySelector("img")?.getAttribute("data-id") || ""
+      )
+    );
+    console.log(visibleSlides);
 
-useEffect(() => {
-  if (
-    navigator.userAgent.indexOf("Safari") != -1 &&
-    navigator.userAgent.indexOf("Chrome") == -1
-  ) {
-    setIsSafari(true);
-  }else{
-    setIsSafari(false);
-  }
-}, []);
-
-const WindowWidth =()=>{
-  const width= window.innerWidth;
-  console.log("Current window width: ", width, " Curr stretch: ", stretch);
-  if(isSafari){
-    if(width > 1200 && stretch !== 190){
-      setStretch(190);
-    }else if(width <= 1200 && stretch !== 10){
-      setStretch(10);
-    }
-  }else{
-    if(width > 1200 && stretch !== 190){
-      setStretch(190);
-    }else if(width <= 1200 && stretch !== 90){
-      setStretch(90);
-    }
-  }
-
-}
-
-useEffect(() => {
-  WindowWidth(); 
-  window.addEventListener('resize', WindowWidth);
-  return () => {
-    window.removeEventListener('resize', WindowWidth);
+    const activeIndex = swiper.realIndex;
+    setCurrentProduct(productsData[activeIndex]);
   };
-}, []);
 
-useEffect(() => {
-  if (swiperInstance) {
-    swiperInstance.update();
-    console.log("stretch:   " + stretch);
-    console.log(swiperInstance);
-  }
-}, [stretch, swiperInstance]);
+  const [isSafari, setIsSafari] = useState(false);
+
+  useEffect(() => {
+    if (
+      navigator.userAgent.indexOf("Safari") != -1 &&
+      navigator.userAgent.indexOf("Chrome") == -1
+    ) {
+      setIsSafari(true);
+    } else {
+      setIsSafari(false);
+    }
+  }, []);
+
+  const WindowWidth = () => {
+    const width = window.innerWidth;
+    console.log("Current window width: ", width, " Curr stretch: ", stretch);
+    if (isSafari) {
+      if (width > 1200 && stretch !== 190) {
+        setStretch(190);
+      } else if (width <= 1200 && stretch !== 10) {
+        setStretch(10);
+      }
+    } else {
+      if (width > 1200 && stretch !== 190) {
+        setStretch(190);
+      } else if (width <= 1200 && stretch !== 90) {
+        setStretch(90);
+      }
+    }
+  };
+
+  useEffect(() => {
+    WindowWidth();
+    window.addEventListener("resize", WindowWidth);
+    return () => {
+      window.removeEventListener("resize", WindowWidth);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (swiperInstance) {
+      swiperInstance.update();
+      console.log("stretch:   " + stretch);
+      console.log(swiperInstance);
+    }
+  }, [stretch, swiperInstance]);
 
   return (
     <div className={styles.first_container} content="f" id="sorta">
       <div className={styles.sliders}>
-      <Swiper
-        key={stretch}
-        onSwiper={setSwiperInstance} 
-        effect={'coverflow'}
-        loop={true}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        spaceBetween={100}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: stretch,
-          depth: 100,
-          modifier: 1,
-          slideShadows: false,
-        }}
-        pagination={{ 
-          clickable: true,
-          dynamicBullets: true,
-        }}
-        navigation
-        mousewheel 
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className={styles.mySwiperBottle}
-        onSlideChange={handleSlideChange}
-      >
-      {images.map((image) => {
-        const isVisible = visibleSlides.includes(image.id); 
-        
-        return (
-          <SwiperSlide key={image.id} style={{ opacity: isVisible ? 1 : 0, }} className={styles.slide}>
-              <img src={image.src} className={styles.img_slide} data-id={image.id} />
-          </SwiperSlide>
-        );
-      })}
+        <Swiper
+          key={stretch}
+          onSwiper={setSwiperInstance}
+          effect={"coverflow"}
+          loop={true}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          spaceBetween={100}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: stretch,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          pagination={{
+            clickable: true,
+            dynamicBullets: true,
+          }}
+          mousewheel
+          modules={[EffectCoverflow, Pagination, Navigation]}
+          className={styles.mySwiperBottle}
+          onSlideChange={handleSlideChange}
+        >
+          {images.map((image) => {
+            const isVisible = visibleSlides.includes(image.id);
 
-      </Swiper>
+            return (
+              <SwiperSlide
+                key={image.id}
+                style={{ opacity: isVisible ? 1 : 0 }}
+                className={styles.slide}
+              >
+                <img
+                  src={image.src}
+                  className={styles.img_slide}
+                  data-id={image.id}
+                />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
       <div className={styles.text_slider}>
         <div className={styles.text_in}>
@@ -190,7 +192,6 @@ useEffect(() => {
         </div>
       </div>
     </div>
-    
   );
 };
 
